d3058686283bf47614fe46a2cdaddd74905ce94b
dhoiffahmad <dhoiff.ahmad@gmail.com>
2023-08-31 21:02:08 +0700

    tambah paragraf lagi

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+< lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Percobaan Git Remote 2</title>
 </head>
 <body>
-    <h1> Halo Kancah Santreh</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsa possimus similique?</p>
+   <h1>Selamat Datang</h1> 
+   
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, distinctio placeat? Distinctio, vero unde quibusdam ipsum veritatis omnis, sit maiores aspernatur quos molestias quidem atque odit libero itaque minima deserunt obcaecati labore!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae at quo molestias nostrum repellendus officiis illo, eveniet beatae iusto, temporibus neque!</p>
 </body>
-</html>
+</
