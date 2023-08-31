55d00ec2780e4e35c666066c8bfec8ca36e05282
dhoiffahmad <dhoiff.ahmad@gmail.com>
2023-08-31 19:26:41 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Percobaan Git Remote 2</title>
+</head>
+<body>
+    <h1> Halo Kancah Santreh</h1>
+</body>
+</html>
